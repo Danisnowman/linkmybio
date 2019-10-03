00cd49283885d800791747e19d3fb7f7cb804ad2
danisnowman <danisnowman@gmail.com>
2019-10-03 11:54:51 -0600

    fixed linktree lol

--- bio.py
+++ bio.py
@@ -1,24 +1,44 @@
 #FLASK
 from flask import Flask, jsonify, render_template, request
 app = Flask(__name__)
+from social_media import *
 import os,optparse
 import yaml
 
 environment=os.getenv("ENVIRONMENT", "development")
+
+social_media_list = []
 
 with open("links.yaml", 'r') as stream:
     try:
         links = yaml.safe_load(stream)
     except yaml.YAMLError as exc:
         print(exc)
+link_list = links.get("links")
+
+for i in range(len(link_list)):
+    # print(link_list[i].get(i+1).get("name"))
+    name = link_list[i].get(i+1).get("name")
+    enable = link_list[i].get(i+1).get("enable")
+    link = link_list[i].get(i+1).get("link")
+    description = link_list[i].get(i+1).get("description")
+
+    social_media = SocialMedia(name, enable, link, description)
+    social_media_list.append(social_media)
+
+
+
 
 @app.route("/")
 def home():
-    return render_template("home.html", links=links)
+    return render_template("home.html",  social_media_list=social_media_list, links=links)
 
 
 if __name__ == "__main__":
     debug=False
     if environment == "development" or environment == "local":
         debug=True
+    link_list = links.get("links")
+
+
     app.run(host="0.0.0.0",debug=debug)
---
+++ social_media.py
@@ -0,0 +1,8 @@
+class SocialMedia():
+    def __init__(self, name, enable, link, description):
+        self.name = name
+        self.enable = enable
+        self.link = link
+        self.description = description
+
+    
